4435b5290014742b15c646e16df9f2d34b27ad60
Cream Starlight <102781612+CreamStarlight@users.noreply.github.com>
2026-09-23 19:33:23 +0800

    Fix, update, create. [0.1.3.0]
    Added a small load of content, and fixed some minor stuff. Now we have more images.

--- lore-menu/script.js
+++ lore-menu/script.js
@@ -1,7 +1,11 @@
+const sb_width="250px"
+
 function sbShow() {
-    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("sidebar").style.width = sb_width;
+    document.getElementById("sidebar").style.borderRight = "2px solid white";
 }
 
 function sbHide() {
     document.getElementById("sidebar").style.width = "0";
+    document.getElementById("sidebar").style.borderRight = "none";
 }
