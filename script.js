0abf17003d49574ae302fc40b1a1d960e9adf87e
Cream Starlight <102781612+CreamStarlight@users.noreply.github.com>
2026-09-23 20:52:27 +0800

    Addition and changes [0.1.3.1]

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ function sbHide() {
     document.getElementById("sidebar").style.width = "0";
     document.getElementById("sidebar").style.borderRight = "none";
 }
+
+//This function might not have many uses as of now.
+function startTime(){
+	var today=new Date();
+
+    var yr=today.getFullYear();
+    var mo=today.getMonth()+1;
+    var da=today.getDate();
+
+	var h=today.getHours();
+	var m=today.getMinutes();
+	var s=today.getSeconds();
+
+    h=checkTime(h);
+	m=checkTime(m);
+	s=checkTime(s);
+
+	document.getElementById('dnt').innerHTML=yr+"/"+mo+"/"+da+" "+h+":"+m+":"+s;
+	t=setTimeout(function(){startTime()},500);
+}
+function checkTime(i){
+	if (i<10){
+		i="0" + i;
+	}
+	return i;
+}
